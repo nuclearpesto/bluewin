ceb5e92bd6befd8420f4748f7f175b7f0bc719e4
jaspenbom <jaspenbom@gmail.com>
2015-04-16 19:06:09 +0100

    Added stack too keep track of free socket array places

--- include/server.h
+++ include/server.h
@@ -1,4 +1,20 @@
 #ifndef THREAD_COUNT
 #define THREAD_COUNT 1000
 #endif
-clients_t* create_client( int count, int socket);
+
+
+struct Stack
+{
+int size;
+int capacity;
+int *current_place;
+};
+typedef struct Stack stack;
+
+clients_t* create_client(int socket, stack *available_client_nr);
+
+void createstack(stack *s, int capacity);
+
+int push(stack *s, int nr);
+
+int pop(stack *s);
--- src/server/server.c
+++ src/server/server.c
@@ -21,23 +21,29 @@
 clients_t clients_arr[THREAD_COUNT];
 int count;
 int main(int argc, char **argv){
-  int listensocket, acceptsocket, port, t; 
+  int listensocket, acceptsocket, port, t;
   struct sockaddr_in local, remote;
   pthread_t thread_ids[THREAD_COUNT];
+stack available_client_nr;
+    int i;
 
-  
   if(argc<2){
     printf("usage: server [port]");
     exit(1);
   }
-  
+
   port=atoi(argv[1]);
   if((listensocket=socket(AF_INET, SOCK_STREAM, 0))==-1)  {
     perror("socket");
     exit(1);
   }
-   
-  
+
+    createstack(&available_client_nr, THREAD_COUNT);
+    for(i=THREAD_COUNT;i>=0;i--)
+    {
+        push(&available_client_nr, i);
+    }
+
   local.sin_family = AF_INET;
   local.sin_port = htons(port);
   local.sin_addr.s_addr=htonl(INADDR_ANY);
@@ -50,25 +56,60 @@ int main(int argc, char **argv){
     perror("listen");
     exit(1);
   }
-  
+
   while(1){
     t=sizeof(remote);
     if((acceptsocket=accept(listensocket,(struct sockaddr *)&remote, &t))==-1){
       perror("accept");
     }
-    clients_t *c=  create_client(count, acceptsocket);
-    pthread_create(&thread_ids[count], NULL, &handle, c); 
+    clients_t *c=  create_client(acceptsocket, &available_client_nr);
+    pthread_create(&thread_ids[count], NULL, &handle, c);
     printf("created thread");
     fflush(stdout);
   }
-  
-    
+
+
 }
 
 
-clients_t* create_client(int count, int socket){
+clients_t* create_client(int socket, stack *available_client_nr){
+    int count = pop(available_client_nr);
   clients_arr[count].inet_addr = 0;
   clients_arr[count].socket = socket;
-  
+
   return &clients_arr[count];
+}
+
+
+void createstack(stack *s,int capacity)
+{
+    s->current_place = (int *)malloc(sizeof(int )*capacity);
+    s->capacity = capacity;
+    s->size = 0;
+}
+
+int push(stack *s, int nr)
+{
+
+    if(s->size == s->capacity)
+    {
+        printf("Stack overflow");
+        return 1;
+    }
+    *(s->current_place) = nr;
+    s->size++;
+    s->current_place++;
+    return 0;
+}
+
+int pop(stack *s)
+{
+    if(s->size == 0)
+    {
+        return -1;
+    }
+    int nr = *(s->current_place);
+    s->size--;
+    s->current_place--;
+    return nr;
 }
