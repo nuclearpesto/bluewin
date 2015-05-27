ba46d8ca4e9f98a3dc315ed95e3d05db6425d629
micke136 <micke136@hotmail.com>
2015-05-27 17:48:27 +0200

    added comment to code
    added source reference in english and some comments about the code

--- src/client/clientcrypt.c
+++ src/client/clientcrypt.c
@@ -1,36 +1,36 @@
-#include <string.h>
+#include <string.h>                    //Kod skriven av Manjinder Singh
 #include <stdlib.h>
 
-void encrypt_cli(unsigned long k[], unsigned long tmp[])       //Den här funktionen är direkt kopierad från boken,
-{                                                              // distributed systems concepts and design fifth edition,
-    unsigned long y = tmp[0],z = tmp[1];                       // författare: George Coulouris, Jean Dollimore, Tim Kindberg och Gordon Blair
-    unsigned long delta = 0x9e3779b9, sum = 0;                 // Publicerad: Maj, 2011
-    int n;                                                     // Krypteringen är designad av: David Wheeler och Roger Needham
+void encrypt_cli(unsigned long k[], unsigned long tmp[])       //<----This function is copied from a book called,
+{                                                              // distributed systems concepts and design fifth edition.
+    unsigned long y = tmp[0],z = tmp[1];                       // Written by George Coulouris, Jean Dollimore, Tim Kindberg och Gordon Blair
+    unsigned long delta = 0x9e3779b9, sum = 0;                 // Published: May, 2011
+    int n;                                                     // the encryption is designed by: David Wheeler and Roger Needham
     for (n=0;n<32;n++)
     {
         sum += delta;
-        y += ((z<<4)+k[0])^(z+sum)^((z>>5)+k[1]);
+        y += ((z<<4)+k[0])^(z+sum)^((z>>5)+k[1]);               // this is the encryption algoritm.
         z += ((y<<4)+k[2])^(y+sum)^((y>>5)+k[3]);
     }
     tmp[0]=y; tmp[1]=z;
 }
 
-int encrypt_Handler(char* msg)                                //den här funktionen är ommodifierad men har tagit inspiration från boken,
-{                                                             //  distributed systems concepts and design fifth edition
-    unsigned long k[4];                                       //
+int encrypt_Handler(char* msg)                                //<------ this function is ispiried by the book that i mention in the function above.
+{
+    unsigned long k[4];
     k[0]=11111111111111;
-    k[1]=22222222222222;
+    k[1]=22222222222222;    //This is a hardcoded key
     k[2]=33333333333333;
     k[3]=44444444444444;
     char tmp[8];
-    int numBlock=0,totalBlock=strlen(msg);
-    int msgLenght=strlen(msg)+1,pad=msgLenght%8;
+    int numBlock=0,totalBlock=strlen(msg);  //totalblock is the size of the string
+    int msgLenght=strlen(msg)+1,pad=msgLenght%8; //we need to pad so the string is devided by 8 without rest
     msgLenght=msgLenght+8-pad;
-    realloc(msg, msgLenght);
+    realloc(msg, msgLenght); // need to allocate more space to this string.
     int i;
     for (i=totalBlock;i<msgLenght;i++)
     {
-        msg[i]=' ';
+        msg[i]=' ';     // padding with spaces
 
     }
     totalBlock=msgLenght/8;
@@ -38,7 +38,7 @@ int encrypt_Handler(char* msg)                                //den här funktion
     {
         for (i=0;i<8;i++)
         {
-            tmp[i]=msg[i+(numBlock*8)];
+            tmp[i]=msg[i+(numBlock*8)];     // move every block to temporary string and encrypt, then move it back to the message string.
         }
         encrypt_cli(k,(unsigned long *)tmp);
         for (i=0;i<8;i++)
@@ -50,25 +50,25 @@ int encrypt_Handler(char* msg)                                //den här funktion
     return totalBlock*8;
 }
 
-void decrypt(unsigned long k[], unsigned long tmp[])           //den här funktionen är också tagen från samma bok som funktionen encrypt.
+void decrypt(unsigned long k[], unsigned long tmp[])           //<--- This function is also copied from the same book as the function encrypt.
 {
     unsigned long y = tmp[0],z = tmp[1];
     unsigned long delta = 0x9e3779b9, sum = delta << 5;
     int n;
     for (n=0;n<32;n++)
     {
-        z -= ((y<<4)+k[2])^(y+sum)^((y>>5)+k[3]);
+        z -= ((y<<4)+k[2])^(y+sum)^((y>>5)+k[3]);                // this is the decryption algoritm.
         y -= ((z<<4)+k[0])^(z+sum)^((z>>5)+k[1]);
         sum -= delta;
     }
     tmp[0]=y;tmp[1]=z;
 }
 
-void decrypt_Handler(char* msg, int size)        // likt encrypt_Handler är denna fukntion inspirerad från boken,
-{                                               /*distributed systems concepts and design fifth edition.*/
+void decrypt_Handler(char* msg, int size)        // <--- as encrypt_Handler, is this fumction inspired by the book that i mention earlier.
+{
     char tmp[8];
     unsigned long k[4];
-    k[0]=11111111111111;
+    k[0]=11111111111111;     // same key as in encryption_Handler.
     k[1]=22222222222222;
     k[2]=33333333333333;
     k[3]=44444444444444;
@@ -79,7 +79,7 @@ void decrypt_Handler(char* msg, int size)        // likt encrypt_Handler är denn
         int i;
         for (i=0;i<8;i++)
         {
-            tmp[i]=msg[i+(numBlock*8)];
+            tmp[i]=msg[i+(numBlock*8)];             // move every block to temporary string and decrypt, then move it back to the message string
         }
         decrypt (k,(unsigned long *)tmp);
         for (i=0;i<8;i++)
